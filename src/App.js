cb720ed93bb6972eb22fc62d3e99b75485021376
abdallah nourAldin <abdallah.nouraldin@gmail.com>
2020-10-21 14:42:53 +0300

    finished-2

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import './App.css';
 class App extends React.Component {
 
   state = {
-    sources: ['https://i.pinimg.com/564x/9a/4e/f4/9a4ef46fe3b1f14ac36eea7412abb03b.jpg',
-      'https://i.pinimg.com/564x/38/7e/3a/387e3a6cc23dabaf84b5d1fb8e262e7a.jpg',
-      'https://i.pinimg.com/564x/ec/e7/a3/ece7a31ac8304159395068bc6dcf0136.jpg',
-      'https://i.pinimg.com/236x/2e/65/15/2e6515e8c846ba6f083c541e46b7b00a.jpg',
-      'https://i.pinimg.com/564x/6b/54/d1/6b54d14ef79e74a694fe91f59925d64c.jpg'
+    sources: ['https://images.pexels.com/photos/2102416/pexels-photo-2102416.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      'https://images.pexels.com/photos/3568518/pexels-photo-3568518.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      'https://images.pexels.com/photos/3184306/pexels-photo-3184306.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      'https://images.pexels.com/photos/3183152/pexels-photo-3183152.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      'https://images.pexels.com/photos/572056/pexels-photo-572056.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
     ]
   }
 
